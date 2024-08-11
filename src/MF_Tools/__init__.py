0f0b00ea2822d45979d8ecd98f4fe2e342f0552f
John Connell <johnconnelltutor@gmail.com>
2024-08-11 15:18:44 -0400

    Finalizing init and toml for first publish

--- src/MF_Tools/__init__.py
+++ src/MF_Tools/__init__.py
@@ -1,27 +1,9 @@
 __version__ = '1.0.0'
 __author__ = 'John Connell - The Mathematic Fanatic'
-__description__ = 'Manim plugin with a variety of helpful utilities'
+__description__ = 'Collection of helpful utilities for Manim'
 
 from .transforms import *
 from .updaters import *
 from .misc import *
 
 #from manim_play_timeline import *
-
-"""
-.expressions contains the main mobject classes of the entire plugin
-.actions contains the animation classes, not currently imported because they are not ready yet
-.utils not imported because they are intended for internal use only
-.nicknames not imported, they are intended to be optionally imported with * if desired.
-
-
-from manim import *
-import manim_smart_algebra as msa
-from manim_smart_algebra.nicknames import * #optional
-
-or something like that depending on preferences. I personally will do
-
-from manim import *
-from manim_smart_algebra import *
-from manim_smart_algebra.nicknames import *
-"""
