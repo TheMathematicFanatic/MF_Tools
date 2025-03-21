5e8661e5f0a3ddd43047f7ab0267c4319183683b
John Connell <johnconnelltutor@gmail.com>
2025-03-20 23:43:11 -0400

    test commit since computer broke :(

--- src/MF_Tools/__init__.py
+++ src/MF_Tools/__init__.py
@@ -2,10 +2,18 @@ __version__ = '1.2.0'
 __author__ = 'John Connell - The Mathematic Fanatic'
 __description__ = 'Collection of helpful utilities for Manim'
 
+
+try:
+    from manim import Scene
+except:
+    from manimlib import Scene
+
+
+
+
 from .transforms import *
 from .updaters import *
 from .geometry import *
 from .misc import *
 from .animations import *
 
-#from manim_play_timeline import *
