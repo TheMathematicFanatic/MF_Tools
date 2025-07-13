9c42f61f69bcfa25131b88f18ee5b9b24a91676c
John Connell <johnconnelltutor@gmail.com>
2025-07-13 17:35:05 -0400

    Improvement to scale_to_fit, returns self to allow chaining

--- src/MF_Tools/rescaling.py
+++ src/MF_Tools/rescaling.py
@@ -6,9 +6,11 @@ def scale_to_fit(mobject:Mobject, len_x=None, len_y=None, len_z=None, buff=0.25)
         mobject_lengths = [mobject.get_width(), mobject.get_height(), mobject.get_depth()]
         scale_values = [(len_-2*buff) / mobject_lengths[i] for i,len_ in enumerate(fit_lengths)]
         mobject.scale(min(scale_values))
+    return mobject
 Mobject.scale_to_fit = scale_to_fit
 
 def scale_to_fit_mobject(mobject:Mobject, other_mobject:Mobject, buff=0.25):
     mobject.scale_to_fit(len_x=other_mobject.get_width(), len_y=other_mobject.get_height(), len_z=other_mobject.get_depth(), buff=buff)
+    return mobject
 Mobject.scale_to_fit_mobject = scale_to_fit_mobject
 
