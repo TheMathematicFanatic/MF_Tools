Fixed scaling functions

src/MF_Tools/rescaling.py
@@ -1,18 +1,7 @@
 from .dual_compatibility import *
 
-def scale_to_fit(mobject:Mobject, len_x=None, len_y=None, len_z=None, buff=0.25, scale_stroke_width=False):
-    fit_lengths = [len_ for len_ in [len_x, len_y, len_z] if len_ is not None and len_ > 1e-10]
-    if any(fit_lengths):
-        mobject_lengths = [mobject.get_width(), mobject.get_height(), mobject.get_depth()]
-        scale_values = [(len_-2*buff) / mobject_lengths[i] for i,len_ in enumerate(fit_lengths)]
-        mobject.scale_with_stroke_width(min(scale_values), scale_stroke_width)
-    return mobject
-Mobject.scale_to_fit = scale_to_fit
 
-def scale_to_fit_mobject(mobject:Mobject, other_mobject:Mobject, **kwargs):
-    mobject.scale_to_fit(len_x=other_mobject.get_width(), len_y=other_mobject.get_height(), len_z=other_mobject.get_depth(), **kwargs)
-    return mobject
-Mobject.scale_to_fit_mobject = scale_to_fit_mobject
+# Monkeypatching all these functions as Mobject methods so that they can be .animated
 
 def maintain_apparent_stroke_width(mobject, camera, recursive=True):
     if len(mobject.submobjects) == 0 or not recursive:
@@ -34,3 +23,41 @@ def scale_with_stroke_width(mobject, scale_factor=1, scale_stroke_width=True):
     mobject.scale(scale_factor)
     return mobject
 Mobject.scale_with_stroke_width = scale_with_stroke_width
+
+
+def scale_to_fit(
+	mobject:Mobject,
+	len_x = None,
+	len_y = None,
+	len_z = None,
+	buff = 0,
+	scaleback = 1,
+	min_scale = None,
+	max_scale = None,
+	scale_stroke_width = False,
+):
+	fit_lengths = [len_ if len_ and len_ > 1e-6 else None for len_ in [len_x, len_y, len_z]]
+	mobject_lengths = [mobject.get_width(), mobject.get_height(), mobject.get_depth()]
+	scale_factors = []
+	for dim in [0,1,2]:
+		if fit_lengths[dim] is not None:
+			scale_value = (fit_lengths[dim]-2*buff) / mobject_lengths[dim] * scaleback
+			scale_value = np.clip(scale_value, min_scale, max_scale)
+			scale_factors.append(scale_value)
+	scale_value = min(scale_factors)
+	scale_with_stroke_width(mobject, scale_value, scale_stroke_width)
+	return mobject
+Mobject.scale_to_fit = scale_to_fit
+
+
+def scale_to_fit_mobject(mobject:Mobject, other_mobject:Mobject, **kwargs):
+    scale_to_fit(
+		mobject,
+		len_x = other_mobject.get_width(),
+		len_y = other_mobject.get_height(),
+		len_z = other_mobject.get_depth(),
+		**kwargs
+	)
+    return mobject
+Mobject.scale_to_fit_mobject = scale_to_fit_mobject
+